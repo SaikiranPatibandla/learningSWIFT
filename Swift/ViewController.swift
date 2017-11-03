76f346e97ca1abfd68e4775c5e02011387f5938f
Saikiran <saikiran@itsd026.local>
2017-11-03 16:01:42 +0530

    Added new view controller

--- Swift/ViewController.swift
+++ Swift/ViewController.swift
@@ -12,6 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let mydict = ["hello": "world",
+                      "0": "a",
+                      "1": "b",
+                      "2": "not a number",
+                      "(1, 2, 3)": "a tuple!"
+            
+            ]as [String: Any]
+        print("sai is ",mydict)
+        
+        let vegetable = "red pepper"
+        switch vegetable {
+        case "celery":
+            print("Add some raisins and make ants on a log.")
+        case "cucumber", "watercress":
+            print("That would make a good tea sandwich.")
+        case let x where x.hasSuffix("pepper"):
+            print("Is it a spicy \(x)?")
+        default:
+            print("Everything tastes good in soup.")
+        }
+
+        print(1.0, 2.0, 3.0, separator: "...", terminator: " ß®END")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
