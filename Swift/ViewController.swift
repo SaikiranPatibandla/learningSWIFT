64aab1fae10ec25b03c290d9a0c8afa8c96766b1
Saikiran <saikiran@itsd026.local>
2017-11-03 11:15:53 +0530

    Initial Commit

---
+++ Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift
+//
+//  Created by Saikiran on 03/11/17.
+//  Copyright © 2017 Inflexion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
